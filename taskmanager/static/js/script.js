e3a5907acac215fce567804dcca7b5ed81300099
lestece <loiceleste94@gmail.com>
2023-02-09 21:18:41 +0000

    template inheritance: base.html created and task homepage

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+// Initialize Materialize sidenav for mobile
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
